40d06d8ddb7e7e769e0471be7187013dbd189bdc
PradecTelecentre <PradecTelecentre>
2020-03-14 20:14:33 +0000

    header du site terminer, en attente de la partie animation

--- header.php
+++ header.php
@@ -11,30 +11,31 @@
  */
 
 ?>
-	<header id="header" class="site__header" role="banner" itemscope itemtype="http://schema.org/WPHeader" style='background:url("<?php echo get_template_directory_uri()?>./resources/assets/src/sass/layouts/header.jpg");background-position: center;background-size: cover;background-repeat: no-repeat;'>
+	<?php if(is_front_page()): ?>
+		<header id="header" class="site__header" role="banner" itemscope itemtype="http://schema.org/WPHeader" style='background:url("<?php echo get_template_directory_uri()?>./resources/assets/src/sass/layouts/header.jpg");background-position: center;background-size: cover;background-repeat: no-repeat;height: 100vh;'>
+	<?php elseif(is_home()): ?>
+		<header id="header" class="site__header" role="banner" itemscope itemtype="http://schema.org/WPHeader" style="background: #3B719F">
+	<?php else: echo '';?>
+	<?php endif; ?>
 		<div class="site-header--primary">
-			<hgroup class="header__title">
-				<h1 itemprop="name">
-					<?php the_custom_logo() ?>
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</h1>
-				<p itemprop="description">
-					<?php bloginfo( 'description' ); ?>
-				</p>
-			</hgroup>
-			<div class="header__link">
-				<?php
-				wp_nav_menu ( array (
-					'theme_location' => 'primary' ,
-					'menu_class' => 'my_primary_class',
-					'menu_id' => 'my_primary_class',
-				) );
-				?>
-			</div>
+		<hgroup class="header__title">
+			<h1 itemprop="name">
+				<?php the_custom_logo() ?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+			</h1>
+		</hgroup>
+		<div class="header__link">
+			<?php
+			wp_nav_menu ( array (
+				'theme_location' => 'primary' ,
+				'menu_class' => 'my_primary_class',
+				'menu_id' => 'my_primary_class',
+			) );
+			?>
 		</div>
-
+		</div>
 		<!-- Add Animation here -->
 
-		
-	</header><!-- .site__header -->
-<div class="space__site">
+			
+		</header><!-- .site__header -->
+		<div class="space__site">
