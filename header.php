bfe6d08cb697c322ff80b76e105fa16a228ba445
PradecTelecentre <PradecTelecentre>
2020-03-21 21:32:54 +0000

    first version still working

--- header.php
+++ header.php
@@ -13,11 +13,6 @@
 ?>
 	<?php if(is_front_page()): ?>
 		<header id="header" class="site__header" role="banner" itemscope itemtype="http://schema.org/WPHeader" style='background:url("<?php echo get_template_directory_uri()?>./resources/assets/src/sass/layouts/header.jpg");background-position: center;background-size: cover;background-repeat: no-repeat;height: 100vh; background-color:#213f58; background-blend-mode:multiply'>
-	<?php elseif(is_home()): ?>
-		<header id="header" class="site__header" role="banner" itemscope itemtype="http://schema.org/WPHeader" style="background: #3B719F">
-	<?php else: ?>
-		<header id="header" class="site__header" role="banner" itemscope itemtype="http://schema.org/WPHeader" style="background: #3B719F">
-	<?php endif; ?>
 		<div class="site-header--primary">
 		<hgroup class="header__title">
 			<h1 itemprop="name">
@@ -35,12 +30,13 @@
 			?>
 		</div>
 		</div>
+		
 		<!-- Add Animation here -->
 		<div class="hero-area">
-				<div id="leftText">
-		<h3> Cybersecurity with Computational and Artificial Intelligence</h3>
-		<p>We make use of computational and artificial intelligence approaches to bring robustness and scalability in the proposed systems</p>
-    </div>
+			<div id="leftText">
+			<h3> Cybersecurity with Computational and Artificial Intelligence</h3>
+			<p>We make use of computational and artificial intelligence approaches to bring robustness and scalability in the proposed systems</p>
+    	</div>
     <div id="container">
         <div class="container-description__left">
             <div id="item_1">
@@ -228,5 +224,62 @@
 		</div>
 
 			
+	<?php elseif(is_home()): ?>
+		<header id="header" class="site__header" role="banner" itemscope itemtype="http://schema.org/WPHeader" style="background: #3B719F">
+		<div class="site-header--primary">
+		<hgroup class="header__title">
+			<h1 itemprop="name">
+				<?php the_custom_logo() ?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+			</h1>
+		</hgroup>
+		<div class="header__link">
+			<?php
+			wp_nav_menu ( array (
+				'theme_location' => 'primary' ,
+				'menu_class' => 'my_primary_class',
+				'menu_id' => 'my_primary_class',
+			) );
+			?>
+		</div>
+		</div>
+	<?php else: ?>
+		<header id="header" class="site__header" role="banner" itemscope itemtype="http://schema.org/WPHeader" style="background: #3B719F">
+		<div class="site-header--primary">
+		<hgroup class="header__title">
+			<h1 itemprop="name">
+				<?php the_custom_logo() ?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+			</h1>
+		</hgroup>
+		<div class="header__link">
+			<?php
+			wp_nav_menu ( array (
+				'theme_location' => 'primary' ,
+				'menu_class' => 'my_primary_class',
+				'menu_id' => 'my_primary_class',
+			) );
+			?>
+		</div>
+		</div>
+	<?php endif; ?>
+		<!-- <div class="site-header--primary">
+		<hgroup class="header__title">
+			<h1 itemprop="name">
+				<?php the_custom_logo() ?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+			</h1>
+		</hgroup>
+		<div class="header__link">
+			<?php
+			//wp_nav_menu ( array (
+				//'theme_location' => 'primary' ,
+				//'menu_class' => 'my_primary_class',
+				//'menu_id' => 'my_primary_class',
+			//) );
+			?>
+		</div>
+		</div> -->
+		
 		</header><!-- .site__header -->
 		<div class="space__site">
